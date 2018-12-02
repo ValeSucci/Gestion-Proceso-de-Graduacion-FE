21bb06e751170516fddd30f867f4b69c54ef1b27
Daniel Mendoza <arceus6666@hotmail.com>
2018-12-02 18:48:14 -0400

    crear docente

--- src/app/crear-docente/crear-docente.component.ts
+++ src/app/crear-docente/crear-docente.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RestapiService } from '../services/restapi.service';
+import { Router } from '@angular/router';
+import { Docente } from '../models/docente';
 
 @Component({
   selector: 'app-crear-docente',
@@ -6,14 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crear-docente.component.css']
 })
 export class CrearDocenteComponent implements OnInit {
+  private docente: Docente;
   pagetitle = 'Docentes';
-  constructor() { }
+  constructor(
+    private _service: RestapiService,
+    private _router: Router
+  ) { 
+    this.docente = new Docente(0,"","",0,"")
+  }
 
   ngOnInit() {
   }
 
   onSubmit(){
-    
+    this._service.postGlobal(this.docente,'/Docente/create','').subscribe(data => {
+
+    }), (err)=>{
+      console.log(err)
+    }
+    this.docente = new Docente(0,"","",0,"")
   }
 
 }
---
+++ src/app/models/docente.ts
@@ -0,0 +1,9 @@
+export class Docente {
+  constructor(
+    public codigo: number,
+    public nombre: string,
+    public direccion: string,
+    public telefono: number,
+    public correo: string
+  ) { }
+}
